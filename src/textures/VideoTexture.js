e675d0fa46b09779e946709069392ab191b9cf9d
arpu <arnold.putz@gmail.com>
2020-09-03 11:29:19 +0200

    Revert "VideoTexture: Make use of requestVideoFrameCallback()."
    This reverts commit 0cf9a5f636c8d26dd48dc28fbdd613436792bf0f.

--- src/textures/VideoTexture.js
+++ src/textures/VideoTexture.js
@@ -12,21 +12,6 @@ function VideoTexture( video, mapping, wrapS, wrapT, magFilter, minFilter, forma
 
 	this.generateMipmaps = false;
 
-	const scope = this;
-
-	function updateVideo() {
-
-		scope.needsUpdate = true;
-		video.requestVideoFrameCallback( updateVideo );
-
-	}
-
-	if ( 'requestVideoFrameCallback' in video ) {
-
-		video.requestVideoFrameCallback( updateVideo );
-
-	}
-
 }
 
 VideoTexture.prototype = Object.assign( Object.create( Texture.prototype ), {
@@ -38,9 +23,8 @@ VideoTexture.prototype = Object.assign( Object.create( Texture.prototype ), {
 	update: function () {
 
 		const video = this.image;
-		const hasVideoFrameCallback = 'requestVideoFrameCallback' in video;
 
-		if ( hasVideoFrameCallback === false && video.readyState >= video.HAVE_CURRENT_DATA ) {
+		if ( video.readyState >= video.HAVE_CURRENT_DATA ) {
 
 			this.needsUpdate = true;
 
