817e04e0e78beda2c04cb6157f1d2ea124c2d86e
arpu <arnold.putz@gmail.com>
2020-09-16 13:16:25 +0200

    use fixed 18fps for VideoTexture update

--- build/three.js
+++ build/three.js
@@ -28595,7 +28595,8 @@
 		this.magFilter = magFilter !== undefined ? magFilter : LinearFilter;
 
 		this.generateMipmaps = false;
-
+	        this.frameRate = 18;
+	        this.prevTime = 0;
 	}
 
 	VideoTexture.prototype = Object.assign( Object.create( Texture.prototype ), {
@@ -28604,17 +28605,30 @@
 
 		isVideoTexture: true,
 
-		update: function () {
+		update: ( function () {
 
-			var video = this.image;
+		//	this.prevTime = Date.now();
 
-			if ( video.readyState >= video.HAVE_CURRENT_DATA ) {
+			return function () {
 
-				this.needsUpdate = true;
+				var video = this.image;
+
+				if ( video.readyState >= video.HAVE_CURRENT_DATA ) {
+
+					var time = Date.now();
+
+					if ( time - this.prevTime >= ( 1000 / this.frameRate ) ) {
+
+						this.needsUpdate = true;
+						this.prevTime = time;
+
+					}
+
+				}
 
 			}
 
-		}
+		} )()
 
 	} );
 
--- build/three.module.js
+++ build/three.module.js
@@ -28663,7 +28663,8 @@ function VideoTexture( video, mapping, wrapS, wrapT, magFilter, minFilter, forma
 	this.magFilter = magFilter !== undefined ? magFilter : LinearFilter;
 
 	this.generateMipmaps = false;
-
+        this.frameRate = 18;
+        this.prevTime = 0;
 }
 
 VideoTexture.prototype = Object.assign( Object.create( Texture.prototype ), {
@@ -28672,17 +28673,30 @@ VideoTexture.prototype = Object.assign( Object.create( Texture.prototype ), {
 
 	isVideoTexture: true,
 
-	update: function () {
+	update: ( function () {
 
-		const video = this.image;
+	//	this.prevTime = Date.now();
 
-		if ( video.readyState >= video.HAVE_CURRENT_DATA ) {
+		return function () {
 
-			this.needsUpdate = true;
+			var video = this.image;
+
+			if ( video.readyState >= video.HAVE_CURRENT_DATA ) {
+
+				var time = Date.now();
+
+				if ( time - this.prevTime >= ( 1000 / this.frameRate ) ) {
+
+					this.needsUpdate = true;
+					this.prevTime = time;
+
+				}
+
+			}
 
 		}
 
-	}
+	} )()
 
 } );
 
